d4da34519c8bdfa67102577c8c3792c2d016fc43
Graeme Henderson <graeme.189@gmail.com>
2019-09-06 14:05:13 +0100

    Initial commit and Passenger class and test files created

---
+++ src/test/java/PassengerTest.java
@@ -0,0 +1,25 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PassengerTest {
+
+    private Passenger passenger;
+
+    @Before
+    public void setUp() {
+        passenger = new Passenger("Bill Bloggs", 1);
+    }
+
+    @Test
+    public void passengerHasName() {
+        assertEquals("Bill Bloggs", passenger.getName());
+    }
+
+    @Test
+    public void passengerHasBags(){
+        assertEquals(1, passenger.getLuggage());
+    }
+}
+
